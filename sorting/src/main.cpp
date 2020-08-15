aac0e21993ee1e36fd04bc33d59159f45fb9c7af
urandom <urandom43@gmail.com>
2020-08-15 22:26:23 +0400

    minor changes

--- sorting/src/main.cpp
+++ sorting/src/main.cpp
@@ -12,32 +12,31 @@ void printArray(std::vector<int> arr, std::size_t tillPos) {
 }
 
 int main(int argc, char *argv[]) {
-	/*if(argc != 2) {
-		cerr << "./main filename.txt" << endl;
+	if(argc != 2) {
+		cerr << "./main filename.txt\n";
 		return 1;
-	}*/
+	}
 
 	ifstream file;
 	file.open(argv[1]);
 	vector<int> nums; //{22, 12, 122, 2, 1, 200, 0, -32, 10};
 
-	//cout << "Loading numbers into vector...\n";
-	//loadIntoVector(file, nums);
-	//cout << "Numbers loaded successfully\n";
+	cout << "Loading numbers into vector...\n";
+	loadIntoVector(file, nums);
+	cout << "Numbers loaded successfully\n";
 
 	printArray(nums, 10);
 	
 	auto t1 = chrono::high_resolution_clock::now();
 	//insertionSort(nums);
 	//selectionSort(nums);
-	mergeSort(nums, 0, nums.size()-1);
+	//mergeSort(nums, 0, nums.size()-1);
 	//sort(nums.begin(), nums.end()); // std::sort
-	//quickSort(nums, 0, nums.size());
-	auto t2 = chrono::high_resolution_clock::now();
-	
+	quickSort(nums, 0, nums.size());
+	auto t2 = chrono::high_resolution_clock::now();	
 	auto duration = chrono::duration_cast<chrono::microseconds>( t2 - t1 ).count();
 	
-	cout << "Array sorted, took " << duration << " microseconds." << endl;
+	cout << "Array sorted, took " << duration << " microseconds.\n";
 	
 	printArray(nums, 10);
 
